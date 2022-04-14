5e0520005e693e695d56b99460ce0138a77a1f58
Dorii0513 <bobodorii0513@gmail.com>
2022-04-14 22:02:17 +0900

    0414

---
+++ Helloworld/week01/hello.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  Helloworld
+//
+//  Created by dorii on 2022/04/13.
+//
+
+import Foundation
+
+print("Yagom Code Starter")
+
---
+++ Helloworld/week01/main.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//  Helloworld
+//
+//  Created by 김예림 on 2022/04/13.
+//
+
+import Foundation
+
+func plusTwo(x: Int) -> Int {
+    return x + 2
+}
